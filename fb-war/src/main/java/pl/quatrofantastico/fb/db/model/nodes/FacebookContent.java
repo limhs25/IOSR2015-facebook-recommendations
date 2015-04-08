7914f61c3b0e381e1f37d217727c54628b80952c
marek1840 <marek1840@gmail.com>
2015-04-08 16:38:34 +0200

    [IS_001] added friends and interest fields in FacebookUser
    Change-Id: I2daf27d2effb6d8fcf9e2f35749610e52a40bbf9

--- fb-war/src/main/java/pl/quatrofantastico/fb/db/model/nodes/FacebookContent.java
+++ fb-war/src/main/java/pl/quatrofantastico/fb/db/model/nodes/FacebookContent.java
@@ -1,6 +1,7 @@
-package pl.quatrofantastico.fb.db.model;
+package pl.quatrofantastico.fb.db.model.nodes;
 
 import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.Indexed;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 
 @NodeEntity
@@ -8,12 +9,18 @@ public class FacebookContent {
     @GraphId
     private Long id;
 
+    @Indexed(unique = true)
     private String name;
 
     @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
     public boolean equals(Object o) {
-        if(this == o)return true;
-        if(!(o instanceof FacebookContent)) return false;
+        if (this == o) return true;
+        if (!(o instanceof FacebookContent)) return false;
         return ((FacebookContent) o).name.equals(name);
     }
 
